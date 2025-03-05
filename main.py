3f570c9e5f1c3cd5ee8b04144d456da5995d7f3e
Tussa Domingos <119713083+BaliNwen@users.noreply.github.com>
2025-03-05 12:57:50 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,26 @@
+game.splash("Welcome to the smoothie maker!")
+game.splash("you get to make your own smoothie!")
+game.splash("Here are your ingredients!")
+froots = ["blueberry",
+    "mango",
+    "peach",
+    "watermelon",
+    "blackberry",
+    "banana",
+    "pinapple"]
+game.splash("blueberry, mango, peach, watermelon, blackberry, banana, pinapple")
+game.ask_for_string("what would you like to add to your drink?")
+
+def froot_selector(froots: str): 
+    for x in froots:
+        str = froots
+
+if froot_selector(""):
+    for x in froots:  
+        pass
+
+
+
+
+
+
---
+++ main.ts
@@ -0,0 +1,19 @@
+game.splash("Welcome to the smoothie maker!")
+game.splash("you get to make your own smoothie!")
+game.splash("Here are your ingredients!")
+let froots = ["blueberry", "mango", "peach", "watermelon", "blackberry", "banana", "pinapple"]
+game.splash("blueberry, mango, peach, watermelon, blackberry, banana, pinapple")
+game.askForString("what would you like to add to your drink?")
+function froot_selector(froots: string) {
+    let str: string;
+    for (let x of froots) {
+        str = froots
+    }
+}
+
+if (froot_selector("")) {
+    for (let x of froots) {
+        
+    }
+}
+

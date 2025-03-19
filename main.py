cec74b30263f1c64edc4b42f453a81ab0f66cd72
Tussa Domingos <119713083+BaliNwen@users.noreply.github.com>
2025-03-19 08:53:52 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
+# return statement / user input
+def fruit_selector(array_fruit: str):
+    for x in array_fruit:
+        if x == '' in array_fruit:
+            game.splash("You've made a " + "" + "smoothie!")
+        elif x : '' in array_fruit
+        # if value not in list then make them restart
+        game.splash("You didn't select anything!")
+        game.game_over(False)
 game.splash("Welcome to the smoothie maker!")
 game.splash("you get to make your own smoothie!")
 game.splash("Here are your ingredients!")
-array_fruit = ["blueberry", "mango", "peach", "watermelon", "blackberry", "banana", "pineapple"] #array of fruit
-
+array_fruit = ["blueberry",
+    "mango",
+    "peach",
+    "watermelon",
+    "blackberry",
+    "banana",
+    "pineapple"]
+# array of fruit
 game.splash(array_fruit)
-
 fruit_selector(game.ask_for_string("what would you like to add to your drink?"))
-
-
-
-def fruit_selector(array_fruit: str): # if users input inside the list than print the value selected
-    if " " in array_fruit:
-        game.splash("You've made a " + "" + "smoothie!" )
-    elif "" not in array_fruit: # if value not in list then make them restart 
-        game.splash("You didn't select anything!")
-        game.game_over(False)
-
-fruit_selector(game.ask_for_string("what would you like to add to your drink?")) #return statement / user input        
-        
-
-
-# displays fruits
-
--- main.ts
+++ main.ts
@@ -1,20 +1,22 @@
+//  return statement / user input
+function fruit_selector(array_fruit: string) {
+    for (let x of array_fruit) {
+        if (x == "" && py.In("", array_fruit)) {
+            game.splash("You've made a " + "" + "smoothie!")
+        } else if (x) {
+            array_fruit.indexOf("") >= 0
+        }
+        
+        //  if value not in list then make them restart
+        game.splash("You didn't select anything!")
+        game.gameOver(false)
+    }
+}
+
 game.splash("Welcome to the smoothie maker!")
 game.splash("you get to make your own smoothie!")
 game.splash("Here are your ingredients!")
 let array_fruit = ["blueberry", "mango", "peach", "watermelon", "blackberry", "banana", "pineapple"]
-// array of fruit
+//  array of fruit
 game.splash(array_fruit)
-fruit_selector(game.askForString("what would you like to add to your drink?"))
-function fruit_selector(array_fruit: string) {
-    //  if users input inside the list than print the value selected
-    if (array_fruit.indexOf(" ") >= 0) {
-        game.splash("You've made a " + "" + "smoothie!")
-    } else if (array_fruit.indexOf("") < 0) {
-        //  if value not in list then make them restart 
-        game.splash("You didn't select anything!")
-        game.gameOver(false)
-    }
-    
-}
-
 fruit_selector(game.askForString("what would you like to add to your drink?"))

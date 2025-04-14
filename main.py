00b577c23ce00602aea4d77b0c7b45948b57c27b
Tussa Domingos <119713083+BaliNwen@users.noreply.github.com>
2025-04-14 13:52:16 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
+# (glorified print statement) project
 def fruit_selection(array_fruit: List[any]):
-    global found
+    global encontro
     choice = game.ask_for_string("what kind of smoothie would you like to make?").to_lower_case()
+    # makes it auto lowercase
     # loop through each fruit in the list
     for fruit in array_fruit:
         if fruit == choice:
-            found = True
-            game.splash("you've made a " + str(fruit) + " smoothie!")
+            encontro = True
+            game.splash("you've made a " + ("" + str(fruit)) + " smoothie!")
             game.splash("goodbye.")
             game.game_over(True)
     # if no match found: error msg = true
-    if not (found):
-        game.splash("we dont have " + choice + "." + "Please choose something else. ")
-        game.splash("stop trolling and choose something.")
+    if not (encontro):
+        game.splash("we dont have " + choice + "." + "please choose something else. ")
+        game.splash("stop trolling and choose something weirdo.")
         game.game_over(False)
-found = False
+encontro = False
 game.splash("welcome to the smoothie maker ladies and gentlemen.")
-game.splash("you may commence your smoothie making")
+game.splash("you may commence your smoothie making vermin.")
 froots = ["blueberry",
     "mango",
     "peach",
@@ -24,5 +26,5 @@ froots = ["blueberry",
     "banana",
     "pineapple"]
 game.splash("here are your ingredients: blueberry, mango, peach, watermelon, blackberry, banana, pineapple")
-game.splash("please choose an option to choose from.")
+game.splash("please choose an option to make a smoothie.")
 fruit_selection(froots)
--- main.ts
+++ main.ts
@@ -1,29 +1,31 @@
+//  (glorified print statement) project
 function fruit_selection(array_fruit: any[]) {
     
     let choice = game.askForString("what kind of smoothie would you like to make?").toLowerCase()
+    //  makes it auto lowercase
     //  loop through each fruit in the list
     for (let fruit of array_fruit) {
         if (fruit == choice) {
-            found = true
-            game.splash("you've made a " + ("" + fruit) + " smoothie!")
+            encontro = true
+            game.splash("you've made a " + ("" + ("" + fruit)) + " smoothie!")
             game.splash("goodbye.")
             game.gameOver(true)
         }
         
     }
     //  if no match found: error msg = true
-    if (!found) {
-        game.splash("we dont have " + choice + "." + "Please choose something else. ")
-        game.splash("stop trolling and choose something.")
+    if (!encontro) {
+        game.splash("we dont have " + choice + "." + "please choose something else. ")
+        game.splash("stop trolling and choose something weirdo.")
         game.gameOver(false)
     }
     
 }
 
-let found = false
+let encontro = false
 game.splash("welcome to the smoothie maker ladies and gentlemen.")
-game.splash("you may commence your smoothie making")
+game.splash("you may commence your smoothie making vermin.")
 let froots = ["blueberry", "mango", "peach", "watermelon", "blackberry", "banana", "pineapple"]
 game.splash("here are your ingredients: blueberry, mango, peach, watermelon, blackberry, banana, pineapple")
-game.splash("please choose an option to choose from.")
+game.splash("please choose an option to make a smoothie.")
 fruit_selection(froots)
